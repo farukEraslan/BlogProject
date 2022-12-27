01d0ae65849ff17c2f1f5c644140ee94d11c891f
farukEraslan <frkersln@gmail.com>
2022-12-27 22:01:49 +0300

    Add project files.

---
+++ BlogProject.Core/Map/CoreMap.cs
@@ -0,0 +1,26 @@
+﻿using BlogProject.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Core.Map
+{
+    public class CoreMap<T> : IEntityTypeConfiguration<T> where T : CoreEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Status).IsRequired(true);
+            builder.Property(e => e.CreatedDate).IsRequired(false);
+            builder.Property(e => e.CreatedComputerName).IsRequired(false).HasMaxLength(255);
+            builder.Property(e => e.CreatedIP).IsRequired(false).HasMaxLength(15);
+            builder.Property(e => e.ModifiedDate).IsRequired(false);
+            builder.Property(e => e.ModifiedComputerName).IsRequired(false).HasMaxLength(255);
+            builder.Property(e => e.ModifiedIP).IsRequired(false).HasMaxLength(15);
+        }
+    }
+}
---
+++ BlogProject.Core/Service/ICoreService.cs
@@ -0,0 +1,28 @@
+﻿using BlogProject.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Core.Service
+{
+    public interface ICoreService<T> where T : CoreEntity
+    {
+        bool Add(T item);
+        bool Add(List<T> items);
+        bool Remove(T item);
+        bool Remove(Guid id);
+        bool RemoveAll(Expression<Func<T, bool>> predicate); // Belli bir LINQ ifadesine göre filtreleyip silmek için yazılan servis metodu. Metodun içine LINQ ifadesi verilecektir.
+        bool Update(T item);
+        T GetById(Guid id);
+        T GetByDefault(Expression<Func<T, bool>> predicate); // FirstOrDefault'a benzer bir metot oluşturur.
+        List<T> GetActive();
+        List<T> GetDefault(Expression<Func<T, bool>> predicate);
+        List<T> GetAll();
+        bool Activate(Guid Id); // Aktifleştirmek için kullanılacak metot.
+        bool Any(Expression<Func<T, bool>> predicate); // LINQ ifadesi ile var mı diye sorgulama yapacağımız metot.
+        int Save(); // DB'de manipülasyon işleminden sonra 1 veya daha fazla satır eklendiğinde bize kaç satırın etkilendiğini döndürecek metot
+    }
+}
