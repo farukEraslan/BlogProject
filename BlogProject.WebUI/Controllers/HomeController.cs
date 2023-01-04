e5a4f084eaf7a522117a6306fa70e9f8250010d2
farukEraslan <frkersln@gmail.com>
2023-01-04 21:50:43 +0300

    Kategori controller eklendi. Home controller'a actionlar eklendi.

--- BlogProject.WebUI/Controllers/HomeController.cs
+++ BlogProject.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using BlogProject.WebUI.Models;
+﻿using BlogProject.Core.Service;
+using BlogProject.Entities.Entities;
+using BlogProject.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,20 +9,38 @@ namespace BlogProject.WebUI.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICoreService<User> _userService;
+        private readonly ICoreService<Post> _postService;
+        private readonly ICoreService<Category> _catService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICoreService<Category> catService, ICoreService<User> UserService, ICoreService<Post> postService)
         {
             _logger = logger;
+            _catService  = catService;
+            _userService = UserService;
+            _postService = postService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Aktif olan postları döndürelim.
+            return View(_postService.GetActive());
         }
 
-        public IActionResult Privacy()
+        public IActionResult PostByCatId(Guid catId)
         {
-            return View();
+            // Kategori Id'ye göre aktif postları döndürelim.
+            return View(_postService.GetDefault(e => e.CategoryId == catId));
+        }
+
+        public IActionResult Post(Guid postId)
+        {
+            // Post'u göstereceğiz. Gösterirkende okunma sayısını(ViewCount) 1 arttıralım.
+            Post okunanPost = _postService.GetById(postId);
+            okunanPost.ViewCount++;
+            _postService.Update(okunanPost);
+
+            return View(_postService.GetById(postId)); // View'a döndürürken ilgili postu, kategorisini, yazarını(kullanıcıyı) döndürmemiz gerekecektir (birden fazla model). Bu sebeple "Tuple" ya da "ViewModel" yapısını kullanmalıyız.
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- BlogProject.WebUI/Program.cs
+++ BlogProject.WebUI/Program.cs
@@ -1,8 +1,15 @@
+﻿using BlogProject.Core.Service;
+using BlogProject.Service.Base;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddControllersWithViews();
 
+// .Net Core MVC de tamamen Dependency Injection yapısıyla çalışıyoruz. ICoreService Interface'inin BaseService ile olan gevşek bağımlılığını tanımlıyoruz. Nerede ICoreService çağırılırsa, onun yerine BaseService gönderilecektir.
+builder.Services.AddScoped(typeof(ICoreService<>), typeof(BaseService<>);
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
